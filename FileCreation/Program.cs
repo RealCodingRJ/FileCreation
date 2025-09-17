de7298b607e3a8ea2abdf59161f8a6eb4b45385e
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-17 19:06:48 -0400

    Add project files.

---
+++ FileCreation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FileCreation
+{
+
+    class Program
+    {
+
+        private static void Main(string[] _)
+        {
+            Console.WriteLine("Enter File Name: ");
+
+            var fileName = Console.ReadLine();
+
+            Task task = new(() =>
+            {
+
+                if (!File.Exists(fileName))
+                {
+                    File.Create(fileName ?? "");
+                    Console.WriteLine("File Created");
+                }
+
+            });
+
+            task.Start();
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
